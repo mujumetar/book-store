7338ca2d040cd2ed1db62dfb2e87cda746b83b75
WebersIO <muzammilmetar82@gmail.com>
2025-06-26 15:52:46 +0530

    completed

--- app.js
+++ app.js
@@ -145,5 +145,5 @@ app.get('/*"*"', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log("Book store app running at http://localhost:3000");
+  console.log("Book store app running at:" + "3000");
 });
